70e953acb9a68299b5cce632cd61a17786dbc1a4
parastooveisi <parastoo.veisi@gmail.com>
2020-01-16 12:58:02 -0600

    Update

--- ethereum/tracker.js
+++ ethereum/tracker.js
@@ -4,7 +4,7 @@ import AssetTracker from "./build/AssetTracker.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(AssetTracker.interface), //contract ABI
-  "0x5a702bcb64Fe7c6Eba7B7ae0cFF3FacC422c3E82" //the address that we deployed our contract on
+  "0x47DeF69d8afa028e7a9EcFDfbD8E041821E9187d" //the address that we deployed our contract on
 );
 
 export default instance;
--- pages/assets/history.js
+++ pages/assets/history.js
@@ -1,59 +1,139 @@
 import React, { Fragment, Component } from "react";
 import { render } from "react-dom";
+import Layout from "../../components/Layout";
+import { Card, Button, Table } from "semantic-ui-react";
+import tracker from "../../ethereum/tracker";
+import AssetRow from "../../components/AssetRow";
+import Geocode from "react-geocode";
+import GoogleMapReact from 'google-map-react';
+import Polyline from 'google-map-react';
 
-import { Timeline, Event } from "react-timeline-scribble";
 
 class History extends Component {
-render(){
- return(
+  static defaultProps = {
+    center: {
+      lat: 52.13,
+      lng: -106.66
+    },
 
-  <Fragment>
-    <h1>React Timeline Scribble</h1>
-    <Timeline>
-      <Event interval={"2016 – 2018"} title={"Lorem"} subtitle={"Ipsum"}>
-        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-        id est laborum.
-      </Event>
-      <Event interval={"2015 – 2016"} title={"Lorem"} subtitle={"Ipsum"}>
-        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-        id est laborum.
-      </Event>
+    zoom: 6
+  };
 
-    <Event interval={"2016 – 2018"} title={"Lorem"} subtitle={"srf"}>
-      dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-      id est laborum.
-    </Event>
-    <Event interval={"2016 – 2018"} title={"Lorem"} subtitle={"srf"}>
-      dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-      id est laborum.
-    </Event>
-    </Timeline>
-  </Fragment>
-);
+
+
+  static async getInitialProps(props) {
+    //const myLatLng = {lat: 52.13, lng: -106.66};
+    const lengthOwners = await tracker.methods.lengthOwners(props.query.id).call();
+    let addresscounts = await tracker.methods.zipCounts().call()
+
+    let owners = await Promise.all(
+      Array(parseInt(lengthOwners))
+        .fill()
+        .map((element, index) => {
+          return tracker.methods.owners(props.query.id,index).call();
+        })
+    );
+    console.log(owners);
+
+    let addressArray = await Promise.all(
+      Array(parseInt(addresscounts))
+        .fill()
+        .map((element, index) => {
+          return tracker.methods.zipcodes(props.query.id,index).call();
+        })
+    );
+
+  const myLatLng = addressArray.map(coords => {
+    const [lat, lng] = coords.split(',').map(Number);
+    return {
+      lat,
+      lng
+    };
+  });
+
+//console.log(result)
+
+    const address = {addressArray}
+
+  return {
+    lengthOwners,
+    owners,
+    myLatLng
+  };
 }
+renderMarkers(map, maps) {
+
+
+  let i;
+  for(i = 0; i < this.props.myLatLng.length; i++){
+  let marker = new maps.Marker({
+    position: this.props.myLatLng[i],
+    map,
+    title: 'Hello World!' + i
+  });
+  let infowindow = new google.maps.InfoWindow({
+    content: "Owner" + (i + 1) + ": " + this.props.owners[i]
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  }
+
+}
+
+renderPolylines (map, maps) {
+    /** Example of rendering geodesic polyline */
+    let geodesicPolyline = new maps.Polyline({
+      path: this.props.myLatLng,
+      geodesic: true,
+      strokeColor: '#00a1e1',
+      strokeOpacity: 1.0,
+      strokeWeight: 4,
+      icons: [{
+        icon: {path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW},
+        offset: '100%',
+        repeat: '20px'
+    }]
+    })
+    geodesicPolyline.setMap(map)
+  }
+
+
+renderowners() {
+  const items = this.props.owners.map(address => {
+    return {
+      header: address,
+      description: "",
+      meta: "Agriculture",
+      fluid: true
+    };
+  });
+
+  return <Card.Group items={items} />;
+}
+  render(){
+
+   return(
+     <Layout>
+     <h3> List of all previous owners! </h3>
+     <div>
+     {this.renderowners()}
+     </div>
+     <div style={{ height: '70vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDNA-2TGMfLQn_dIcr0gUNI6gz93qMxHDk' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          onGoogleApiLoaded={({map, maps}) => (this.renderMarkers(map, maps), this.renderPolylines(map, maps))}
+        >
+
+        </GoogleMapReact>
+       </div>
+     </Layout>
+  );
+  }
 }
 export default History;
 
-// if (typeof window !== 'undefined') {
-//     React.render(<App />, document.getElementById("root"));
-// }
+
+///          onGoogleApiLoaded={({map, maps}) => this.renderPolylines(map, maps)}
