96a64b19eca2aba8af439ce07054651d289af6a2
parastooveisi <parastoo.veisi@gmail.com>
2021-02-02 11:59:42 -0600

    Improve the smart contract

--- ethereum/contracts/AssetTracker.sol
+++ ethereum/contracts/AssetTracker.sol
@@ -1,74 +1,76 @@
 pragma solidity ^0.4.18;
+
 contract AssetTracker {
-    uint public id;
-    uint nonce;
-    uint public value;
-    /* address public seller;
-    address public buyer; */
-    mapping(uint => address) public sellerById;
-    mapping(uint => address) public buyerById;
+    uint256 public id;
+    uint256 nonce;
+    uint256 public value;
+
+    mapping(uint256 => address) public sellerById;
+    mapping(uint256 => address) public buyerById;
 
     address[] public producers;
     address[] public buyers;
-    uint public assetsCount;
-    uint public zipCounts;
+    uint256 public assetsCount;
+    uint256 public zipCounts;
 
     struct Asset {
         string name;
         string description;
         string manufacture;
-        uint price;
-        uint id;
+        uint256 price;
+        uint256 id;
         address owner;
         bool initialized;
     }
     Asset[] public assets;
-   // mapping(uint  => Asset) public assetById;
-    mapping(address => mapping(uint => bool)) public walletStore;
-    mapping(uint  => Asset) public assetStore;
+    mapping(address => mapping(uint256 => bool)) public walletStore;
+    mapping(uint256 => Asset) public assetStore;
     mapping(address => bool) public producers_;
-    mapping(uint => address[]) public owners;
-    mapping(uint => string[]) public zipcodes;
-    mapping (address => uint) public arrayIndexes;
+    mapping(uint256 => address[]) public owners;
+    mapping(uint256 => string[]) public zipcodes;
+    mapping(address => uint256) public arrayIndexes;
 
-
-    /* event AssetCreate(address account, uint id, string manufacture);
-    event Transfer(address account, uint id) */
-
-    function createAsset(string name, string description, string manufacture, uint price, string zipcode) public payable{
-
-        id = uint(keccak256(now, msg.sender, nonce)) % 1000;
+    function createAsset(
+        string name,
+        string description,
+        string manufacture,
+        uint256 price,
+        string zipcode
+    ) public payable {
+        id = uint256(keccak256(now, msg.sender, nonce)) % 1000;
         nonce++;
 
-        /* sellerById[id] = msg.sender; */
         sellerById[id] = msg.sender;
-        if(!producers_[msg.sender]){
+        if (!producers_[msg.sender]) {
             producers.push(msg.sender);
             producers_[msg.sender] = true;
         }
 
         value = msg.value / 2;
-        /* require((2 * value) == msg.value);  */
-        Asset memory newAsset = Asset(name, description, manufacture, price, id, msg.sender, true);
-        assetStore[id] = Asset(name, description, manufacture, price, id, msg.sender, true);
+        Asset memory newAsset =
+            Asset(name, description, manufacture, price, id, msg.sender, true);
+        assetStore[id] = Asset(
+            name,
+            description,
+            manufacture,
+            price,
+            id,
+            msg.sender,
+            true
+        );
         assets.push(newAsset);
         assetsCount++;
         owners[id].push(msg.sender);
         zipcodes[id].push(zipcode);
         zipCounts++;
         walletStore[msg.sender][id] = true;
-        uint index = assets.length;
+        uint256 index = assets.length;
         arrayIndexes[msg.sender] = index;
-        //assetById[id] = newAsset;
     }
-//fix owners array --> id all previous owners
 
-    function transferProduct(uint id, string zipcode) public payable{
-
-      // require(assets[id].price == msg.value);
-
+    function transferProduct(uint256 id, string zipcode) public payable {
         require(assetStore[id].initialized);
-        uint idx = (arrayIndexes[msg.sender]);
+        uint256 idx = (arrayIndexes[msg.sender]);
         buyerById[id] = msg.sender;
         walletStore[assetStore[id].owner][id] = false;
         walletStore[msg.sender][id] = true;
@@ -78,54 +80,45 @@ contract AssetTracker {
         assetStore[id].owner = msg.sender;
         assets[idx].owner = msg.sender;
         owners[id].push(msg.sender);
-
     }
 
-    function confirmReceived(uint id) public
-    {
+    function confirmReceived(uint256 id) public {
         buyerById[id].transfer(value);
         sellerById[id].transfer(address(this).balance);
     }
 
-    /* function abort() public onlySeller
-    {
-        seller.transfer(address(this).balance);
-    } */
-
-    modifier onlyProducerCanCall(){
+    modifier onlyProducerCanCall() {
         require(producers_[msg.sender]);
         _;
     }
 
-    function getproducers() public view returns(address[]){
-      return producers;
-    }
-    function getbuyers() public view returns(address[]){
-      return buyers;
+    function getproducers() public view returns (address[]) {
+        return producers;
     }
 
-    function lengthOwners(uint id) public view returns(uint) {
+    function getbuyers() public view returns (address[]) {
+        return buyers;
+    }
+
+    function lengthOwners(uint256 id) public view returns (uint256) {
         return owners[id].length;
     }
 
-    /* function lengthZipcodes(uint id) public view returns(uint) {
-        return zipcodes[id].length;
-    } */
-
-    function getid() public view returns(uint){
-      return id;
-    }
-    function random() private returns (uint) {
-      uint random = uint(keccak256(now, msg.sender, nonce)) % 1000;
-      nonce++;
-      return random;
+    function getid() public view returns (uint256) {
+        return id;
     }
 
-    function getRandom() public view returns (uint) {
+    function random() private returns (uint256) {
+        uint256 random = uint256(keccak256(now, msg.sender, nonce)) % 1000;
+        nonce++;
+        return random;
+    }
+
+    function getRandom() public view returns (uint256) {
         return random();
     }
 
-    function getOwners(uint id) public view returns (address[]) {
+    function getOwners(uint256 id) public view returns (address[]) {
         return owners[id];
     }
     /* modifier onlyBuyer() {
@@ -137,8 +130,4 @@ contract AssetTracker {
         require(msg.sender == seller);
         _;
     } */
-
-
-
-
 }
