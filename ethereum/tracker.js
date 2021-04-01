31552e982b2fe3a3b2221aec8509de104b52b5a2
parastooveisi <parastoo.veisi@gmail.com>
2021-03-31 18:43:54 -0600

    Recompile the smart contract and deploy it with new address

--- ethereum/tracker.js
+++ ethereum/tracker.js
@@ -4,7 +4,7 @@ import AssetTracker from "./build/AssetTracker.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(AssetTracker.interface), //contract ABI
-  "0x23b401DaF1cd1Bff0CFbaD393B343F46DC6f0fbb" //the address that we deployed our contract on
+  "0x8Ad05e5947a0F6F55fD4910db846E0Df9e7Eb687" //the address that we deployed our contract on
 );
 
 export default instance;
